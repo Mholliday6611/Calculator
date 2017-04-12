694d62db2df0fb668fe72f9ac9d34018cd5d44f0
mholliday6611 <mholliday6611@gmail.com>
2017-04-11 20:54:35 -0400

    first push

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function add (){
+	//console.log("let's add");
+	var math = 7 + 7
+	console.log(math);
+}
+function subtract (){
+	//console.log("let's take aways");
+	var math = 9 - 3
+	console.log(math);
+}
+function multiply (){
+	//console.log("let's multiply");
+	var math = 10 * 3
+	console.log(math);
+}
+function divide (){
+	//console.log("let's divide");
+	var math= 45 / 5
+	console.log(math);
+}
